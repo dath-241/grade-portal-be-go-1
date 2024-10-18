fe7a60b5bc3c667383fe2eb6b346054b4afc025d
thinhtruong2109 <thinh.truong21092004@hcmut.edu.vn>
2024-10-18 16:48:01 +0700

    Controller-account
    Khong sua gi

--- routes/admin/teacher_route.go
+++ routes/admin/teacher_route.go
@@ -3,5 +3,8 @@ package routes_admin
 import "github.com/gin-gonic/gin"
 
 func TeacherRoute(r *gin.RouterGroup) {
-	// r.POST("/create", controller_admin.CreateTeacher) Khong tao rieng sinh vien hoac giao vien. Chi tao account
+	//r.POST("/create", controller_admin.CreateTeacher) Khong tao rieng sinh vien hoac giao vien. Chi tao account
+	// r.GET("/:id", controller_admin.GetTeacher)
+	// r.PUT("/:id", controller_admin.UpdateTeacher)
+	// r.DELETE("/:id", controller_admin.DeleteTeacher)
 }
