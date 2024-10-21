534a0949e1725ddfe2f78964119e6b25a604cbaa
Thaily <lyvinhthai321@gmail.com>
2024-10-21 23:38:55 +0700

    update client

--- src/controllers/client/interface.go
+++ src/controllers/client/interface.go
@@ -4,3 +4,19 @@ type InterfaceUserController struct {
 	IDToken string `json:"idToken"`
 	Role    string `json:"role"`
 }
+
+type InterfaceScoreController struct {
+	BT  []float32 `json:"BT"`
+	TN  []float32 `json:"TN"`
+	BTL []float32 `json:"BTL"`
+	GK  float32   `json:"GK"`
+	CK  float32   `json:"CK"`
+}
+
+type InterfaceResultScoreController struct {
+	SCORE []struct {
+		MSSV string                   `json:"MMSV"`
+		Data InterfaceScoreController `json:"Data"`
+	} `json:"score"`
+	ClassID string `json:"class_id"`
+}
