b71cc01fd534328880688bd30d9236a93120fbea
Thaily <lyvinhthai321@gmail.com>
2024-11-03 18:00:57 +0700

    update cors

--- src/main.go
+++ src/main.go
@@ -6,7 +6,9 @@ import (
 	routes_client "LearnGo/routes/client"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -22,14 +24,14 @@ func main() {
 	app := gin.Default()
 
 	// Cấu hình CORS
-	// app.Use(cors.New(cors.Config{
-	// 	AllowAllOrigins:  true, // Cho phép tất cả mọi người truy cập
-	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-	// 	ExposeHeaders:    []string{"Content-Length"},
-	// 	AllowCredentials: true,
-	// 	MaxAge:           12 * time.Hour,
-	// }))
+	app.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true, // Cho phép tất cả mọi người truy cập
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Đăng ký các route
 	routes_admin.MainRoute(app)
