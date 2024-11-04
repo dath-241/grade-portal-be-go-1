b6974175091d94221b128234486b266094b0ec6e
Thaily <lyvinhthai321@gmail.com>
2024-11-04 20:48:09 +0700

    update cors

--- src/controllers/admin/auth_controller.go
+++ src/controllers/admin/auth_controller.go
@@ -42,7 +42,6 @@ func LoginController(c *gin.Context) {
 		return
 	}
 	token := helper.CreateJWT(user.ID)
-	fmt.Println(token)
 	c.SetCookie("token", token, 3600*24, "/", "test-jcz3.vercel.app", true, true)
 	c.JSON(200, gin.H{
 		"code":  "Success",
